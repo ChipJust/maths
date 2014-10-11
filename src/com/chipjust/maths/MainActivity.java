6801a41d04c689812b22906d4bb7a19ebf404fdf
ChipJust <chipjust@gmail.com>
2014-10-10 20:10:11 -0700

    Added (and commented out) a basic drawable for button borders. Change background color for selected user. Added support to make a current user. Fixed bug, was using == instead of .equal for string compares.

--- src/com/chipjust/maths/MainActivity.java
+++ src/com/chipjust/maths/MainActivity.java
@@ -12,6 +12,7 @@ import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -33,6 +34,8 @@ public class MainActivity extends Activity {
 	
 	private static final String USER_LIST = "user_list";
 	private static final String NEW_USER = "New User";
+	private static final String CURRENT_USER = "Current User";
+	private static final String COLOR_SELECTED_USER = "#D9E3B1";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -80,12 +83,20 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				Button b = (Button)v;
+				Button b = (Button) v;
 			    String buttonText = b.getText().toString();
-				Log.v(TAG, String.format("onClick:%s", buttonText));
-				if (buttonText == NEW_USER) {
+				Log.v(TAG, String.format("onClick:%s.", buttonText));
+				if (buttonText.equals(NEW_USER)) {
 					getFragmentManager().beginTransaction().replace(R.id.container, new NewUserFragment()).commit();
+					return;
 				}
+
+				// Change the current user to this user.
+				SharedPreferences.Editor editor = getActivity().getPreferences(Context.MODE_PRIVATE).edit();
+				editor.putString(CURRENT_USER, buttonText);
+				editor.commit();
+				
+				//BUGBUG: Display the fragment for this user.
 			}
 			
 		}
@@ -97,9 +108,12 @@ public class MainActivity extends Activity {
 
 			SharedPreferences pref = getActivity().getPreferences(Context.MODE_PRIVATE);
 			Set<String> user_list = pref.getStringSet(USER_LIST, new HashSet<String>());
+			
 			// Add the New User psuedo-user to the set. We can always add more users.
 			user_list.add(NEW_USER);
 			//NEWREL: Convert to list and sort.
+			
+			String currentUser = pref.getString(CURRENT_USER, "");
 			
 			// Create a button for each user.
 			for (Object user: user_list) {
@@ -107,6 +121,10 @@ public class MainActivity extends Activity {
 				button.setText((CharSequence) user);
 				button.setOnClickListener(new UserButtonListener());
 				//NEWREL: Check to see if this button is for the currently selected user. If so, highlight it.
+				if (user.equals(currentUser)) {
+					//button.setBackground(getResources().getDrawable(R.drawable.button_border));
+					button.setBackgroundColor(Color.parseColor(COLOR_SELECTED_USER));
+				}
 				l.addView(button);
 			}
 
@@ -119,11 +137,11 @@ public class MainActivity extends Activity {
 		EditText input = (EditText) findViewById(R.id.new_user_input);
 		String newUser = input.getText().toString();
 		
-		if (newUser == "") {
+		if (newUser.equals("")) {
 			// NEWREL: Show error.
 			return;
 		}
-		if (newUser ==  NEW_USER) {
+		if (newUser.equals(NEW_USER)) {
 			// NEWREL: Show error.
 			return;
 		}
@@ -145,14 +163,14 @@ public class MainActivity extends Activity {
 
 		// Transition back to the User Selection Screen.
 		getFragmentManager().beginTransaction().replace(R.id.container, new UserSelectionFragment()).commit();
+		return;
 	}
 	
 	public static class NewUserFragment extends Fragment {
-
+		
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_new_user, container, false);
-
 			return rootView;
 		}
 	}
